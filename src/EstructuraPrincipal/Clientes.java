30964bb643c06768d1d862c8bd4c0cec2e0d984e
Edgar Poot Ku <l20070055@valladolid.tecnm.mx>
2022-05-08 12:00:21 -0500

    Se agrega los atributos a clientes

--- src/EstructuraPrincipal/Clientes.java
+++ src/EstructuraPrincipal/Clientes.java
@@ -1,5 +1,9 @@
 package EstructuraPrincipal;
 
 public class Clientes {
-    
+    private long Id;
+    private String nombre;
+    private byte edad;
+    private String ciudad;
+    private String correo;
 }
